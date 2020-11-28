7cee6f3be9d223b27270f9fb6636fe9bfcf3f7c5
Aaron Garner <aaron.garner.1982@gmail.com>
2020-11-28 15:14:43 -0800

    added all 5 die to app.js. Now need to get individual states and buttons for each die

--- src/App.js
+++ src/App.js
@@ -18,7 +18,10 @@ class App extends Component {
     super(props);
     this.state = {
       d1Trigger: "r",
-      d2Trigger: "r"
+      d2Trigger: "r",
+      d3Trigger: "r",
+      d4Trigger: "r",
+      d5Trigger: "r"
     }
   }
 
@@ -39,6 +42,22 @@ toggleD2Disabled = () => {
   }
 }
 
+toggleD3Disabled = () => {
+  if (this.state.d3Trigger === "r") {
+    this.setState({d3Trigger: "a"});
+  } else {
+    this.setState({d3Trigger: "r"})
+  }
+}
+
+toggleD4Disabled = () => {
+  if (this.state.d4Trigger === "r") {
+    this.setState({d4Trigger: "a"});
+  } else {
+    this.setState({d4Trigger: "r"})
+  }
+};
+
   
 
 
@@ -51,6 +70,15 @@ render(){
 
 <Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d2Trigger} />
 <button type="button" id="rollButton" onClick={this.toggleD2Disabled}>Click to roll</button>
+
+<Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d3Trigger} />
+<button type="button" id="rollButton" onClick={this.toggleD3Disabled}>Click to roll</button>
+
+<Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d4Trigger} />
+<button type="button" id="rollButton" onClick={this.toggleD4Disabled}>Click to roll</button>
+
+<Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d5Trigger} />
+<button type="button" id="rollButton" onClick={this.toggleD5Disabled}>Click to roll</button>
 
 
       <header className="App-header">
