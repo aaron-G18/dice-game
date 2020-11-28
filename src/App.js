e60f90cd887ee70d881dc15ae1e6aaf6ae69392b
Aaron Garner <aaron.garner.1982@gmail.com>
2020-11-28 15:27:25 -0800

    Updated App.js to get first button text to toggle between 'Hold' and 'Release.' Got that working.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React, { Component } from "react";
-import logo from './logo.svg';
 import './App.css';
 import Dice from 'react-dice-roll';
-
-
-
-// const rollDice = () => {
-//   let roll = document.createEvent(KeyboardEvent);
-//   roll.initEvent("a", true, true);
-//   document.getElementById("rollButton").dispatchEvent(roll);
-// }
-
 
 
 class App extends Component {
@@ -18,6 +8,7 @@ class App extends Component {
     super(props);
     this.state = {
       d1Trigger: "r",
+      d1ButtonText: "Hold",
       d2Trigger: "r",
       d3Trigger: "r",
       d4Trigger: "r",
@@ -29,8 +20,10 @@ class App extends Component {
 toggleD1Disabled = () => {
   if (this.state.d1Trigger === "r") {
     this.setState({d1Trigger: "a"});
+    this.setState({d1ButtonText: "Release"});
   } else {
-    this.setState({d1Trigger: "r"})
+    this.setState({d1Trigger: "r"});
+    this.setState({d1ButtonText: "Hold"});
   }
 };
 
@@ -73,36 +66,21 @@ render(){
   return (
     <div className="App">
 
-<Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d1Trigger} />
-<button type="button" id="rollButton" onClick={this.toggleD1Disabled}>Click to roll</button>
+      <Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d1Trigger} />
+      <button type="button" id="rollButton" onClick={this.toggleD1Disabled}>{this.state.d1ButtonText}</button>
 
-<Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d2Trigger} />
-<button type="button" id="rollButton" onClick={this.toggleD2Disabled}>Click to roll</button>
+      <Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d2Trigger} />
+      <button type="button" id="rollButton" onClick={this.toggleD2Disabled}>Click to roll</button>
 
-<Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d3Trigger} />
-<button type="button" id="rollButton" onClick={this.toggleD3Disabled}>Click to roll</button>
+      <Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d3Trigger} />
+      <button type="button" id="rollButton" onClick={this.toggleD3Disabled}>Click to roll</button>
 
-<Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d4Trigger} />
-<button type="button" id="rollButton" onClick={this.toggleD4Disabled}>Click to roll</button>
+      <Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d4Trigger} />
+      <button type="button" id="rollButton" onClick={this.toggleD4Disabled}>Click to roll</button>
 
-<Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d5Trigger} />
-<button type="button" id="rollButton" onClick={this.toggleD5Disabled}>Click to roll</button>
+      <Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d5Trigger} />
+      <button type="button" id="rollButton" onClick={this.toggleD5Disabled}>Click to roll</button>
 
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   )};
 }
