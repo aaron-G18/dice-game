1186916d45d8153f776688453177ff02782e8fbc
Aaron Garner <aaron.garner.1982@gmail.com>
2020-11-28 14:31:15 -0800

    added react-dice-roll npm package and put a die in App.js for testing.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
+import Dice from 'react-dice-roll';
 
-function App() {
+
+// const rollDice = () => {
+//   let roll = document.createEvent(KeyboardEvent);
+//   roll.initEvent("a", true, true);
+//   document.getElementById("rollButton").dispatchEvent(roll);
+// }
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+  }
+
+
+  triggerKeyPress() {
+    let spaceEvnt = new KeyboardEvent('keyup', {"key": "a", "code": "kVK_ANSI_A (0x00)"});
+    document.dispatchEvent(spaceEvnt);
+    console.log(spaceEvnt);
+    
+  }
+
+  
+
+
+render(){
   return (
     <div className="App">
+
+<Dice onRoll={(value) => console.log(value)} size="50" triggers="a" />
+<button type="button" id="rollButton" onClick={this.triggerKeyPress}>Click to roll</button>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +51,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )};
 }
 
 export default App;
