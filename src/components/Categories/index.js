482f88d665648632e11df28a027b402879665ac7
Aaron Garner <aaron.garner.1982@gmail.com>
2020-11-30 14:16:56 -0800

    updated categories component (spcing for border of each cat

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -6,10 +6,22 @@ function Categories(props) {
     return(
         <div className="categories">
             <div className="cat">
-                <img className="die-img" src="./images/one.png" alt="1 on die face"></img><h3 className="cat-label">  Aces = 1</h3>
+                <img className="die-img" src="./images/one.png" alt="1 on die face"></img><h3 className="cat-label">   Aces = 1</h3>
             </div>
             <div className="cat">
-                <img className="die-img" src="./images/two.png" alt="2 on die face"></img><h3 className="cat-label">  Twos = 2</h3>
+                <img className="die-img" src="./images/two.png" alt="2 on die face"></img><h3 className="cat-label">   Twos = 2</h3>
+            </div>
+            <div className="cat">
+                <img className="die-img" src="./images/three.png" alt="3 on die face"></img><h3 className="cat-label">   Threes = 3</h3>
+            </div>
+            <div className="cat">
+                <img className="die-img" src="./images/four.png" alt="4 on die face"></img><h3 className="cat-label">   Fours = 4</h3>
+            </div>
+            <div className="cat">
+                <img className="die-img" src="./images/five.png" alt="5 on die face"></img><h3 className="cat-label">   Fives = 5</h3>
+            </div>
+            <div className="cat">
+                <img className="die-img" src="./images/six.png" alt="6 on die face"></img><h3 className="cat-label">   Sixes = 6</h3>
             </div>
             
             
