dcd549e1aed15df9135f92f8fcdd456e10f639f2
Aaron Garner <aaron.garner.1982@gmail.com>
2020-11-30 10:08:44 -0800

    added categories component and imported it on app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import './App.css';
 import Dice from 'react-dice-roll';
+import Categories from "./components/Categories";
 
 
 class App extends Component {
@@ -78,23 +79,26 @@ toggleD5Disabled = () => {
 
 render(){
   return (
-    <div className="App">
+    <div className="app">
+      <div className="dice-container">
+        <Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d1Trigger} />
+        <button type="button" id="rollButton" onClick={this.toggleD1Disabled}>{this.state.d1ButtonText}</button>
 
-      <Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d1Trigger} />
-      <button type="button" id="rollButton" onClick={this.toggleD1Disabled}>{this.state.d1ButtonText}</button>
+        <Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d2Trigger} />
+        <button type="button" id="rollButton" onClick={this.toggleD2Disabled}>{this.state.d2ButtonText}</button>
 
-      <Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d2Trigger} />
-      <button type="button" id="rollButton" onClick={this.toggleD2Disabled}>{this.state.d2ButtonText}</button>
+        <Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d3Trigger} />
+        <button type="button" id="rollButton" onClick={this.toggleD3Disabled}>{this.state.d3ButtonText}</button>
 
-      <Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d3Trigger} />
-      <button type="button" id="rollButton" onClick={this.toggleD3Disabled}>{this.state.d3ButtonText}</button>
+        <Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d4Trigger} />
+        <button type="button" id="rollButton" onClick={this.toggleD4Disabled}>{this.state.d4ButtonText}</button>
 
-      <Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d4Trigger} />
-      <button type="button" id="rollButton" onClick={this.toggleD4Disabled}>{this.state.d4ButtonText}</button>
-
-      <Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d5Trigger} />
-      <button type="button" id="rollButton" onClick={this.toggleD5Disabled}>{this.state.d5ButtonText}</button>
-
+        <Dice onRoll={(value) => console.log(value)} size="50" triggers={this.state.d5Trigger} />
+        <button type="button" id="rollButton" onClick={this.toggleD5Disabled}>{this.state.d5ButtonText}</button>
+      </div>
+      <div className="categories-container">
+        <Categories />
+      </div>
     </div>
   )};
 }
---
+++ src/components/Categories/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Categories(props) {
+    return(
+        <div className="categories">
+            <h1>Categories Here</h1>
+        </div>
+    );
+}
+
+export default Categories;
