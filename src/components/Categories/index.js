689d00b88d54be7031e1721f46f99a96eaae6b12
Aaron Garner <aaron.garner.1982@gmail.com>
2020-11-30 12:27:44 -0800

    added categories component to start to layout front end.

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./style.css";
+import Dice from "react-dice-roll";
 
 function Categories(props) {
     return(
         <div className="categories">
             <h1>Categories Here</h1>
+            <Dice defaultValue="1" triggers="false" size="50" /><p className="apostrophe">  's</p>
         </div>
     );
 }
