a1e1c69fee1a2bc806c623dcc4ff1f661a53705f
Aaron Garner <aaron.garner.1982@gmail.com>
2020-12-03 17:43:36 -0800

    Added notes in Categories to identify the table rows to make it a little easier to find the rows.

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -15,6 +15,7 @@ function Categories(props) {
                     </tr>
                 </thead>
                 <tbody>
+                {/* Aces row */}
                     <tr>
                         <td>
                             <img className="die-img" src="./images/one.png" alt="1 on die face"></img><h3 className="cat-label">   Aces = 1</h3>
@@ -26,6 +27,7 @@ function Categories(props) {
                         <td>Score Here</td>
                         <td>Score Here</td>
                     </tr>
+                {/* Twos row */}
                     <tr>
                         <td>
                             <img className="die-img" src="./images/two.png" alt="2 on die face"></img><h3 className="cat-label">   Twos = 2</h3>
@@ -37,6 +39,7 @@ function Categories(props) {
                         <td>Score Here</td>
                         <td>Score Here</td>
                     </tr>
+                {/* Threes row */}
                     <tr>
                         <td>
                             <img className="die-img" src="./images/three.png" alt="3 on die face"></img><h3 className="cat-label">   Threes = 3</h3>
@@ -46,6 +49,7 @@ function Categories(props) {
                         <td>Score Here</td>
                         <td>Score Here</td>
                     </tr>
+                {/* Fours row */}
                     <tr>
                         <td>
                             <img className="die-img" src="./images/four.png" alt="4 on die face"></img><h3 className="cat-label">   Fours = 4</h3>
@@ -55,6 +59,7 @@ function Categories(props) {
                         <td>Score Here</td>
                         <td>Score Here</td>
                     </tr>
+                {/* Fives row */}
                     <tr>
                         <td>
                             <img className="die-img" src="./images/five.png" alt="5 on die face"></img><h3 className="cat-label">   Fives = 5</h3>
@@ -64,6 +69,7 @@ function Categories(props) {
                         <td>Score Here</td>
                         <td>Score Here</td>
                     </tr>
+                {/* Sixes row */}
                     <tr>
                         <td>
                             <img className="die-img" src="./images/six.png" alt="6 on die face"></img><h3 className="cat-label">   Sixes = 6</h3>
@@ -73,6 +79,7 @@ function Categories(props) {
                         <td>Score Here</td>
                         <td>Score Here</td>
                     </tr>
+                {/* Bonus row */}
                     <tr>
                         <td>
                             <h3 className="cat-label">Bonus</h3>
@@ -83,6 +90,7 @@ function Categories(props) {
                         <td>Score Here</td>
                         <td>Score Here</td>
                     </tr>
+                {/* 3 of a kind row */}
                     <tr>
                         <td>
                             <h3 className="cat-label">3 of a kind</h3> 
@@ -92,6 +100,7 @@ function Categories(props) {
                         <td>Score Here</td>
                         <td>Score Here</td>
                     </tr>
+                {/* 4 of a kind row */}
                     <tr>
                         <td>
                             <h3 className="cat-label">4 of a kind</h3>
@@ -101,6 +110,7 @@ function Categories(props) {
                         <td>Score Here</td>
                         <td>Score Here</td>
                     </tr>
+                {/* Full House row */}
                     <tr>
                         <td>
                             <h3 className="cat-label">Full House</h3>
@@ -110,6 +120,7 @@ function Categories(props) {
                         <td>Score Here</td>
                         <td>Score Here</td>
                     </tr>
+                {/* Sm Straight row */}
                     <tr>
                         <td>
                             <h3 className="cat-label">Sm Straight</h3>
@@ -119,6 +130,7 @@ function Categories(props) {
                         <td>Score Here</td>
                         <td>Score Here</td>
                     </tr>
+                {/* Lg Straight row */}
                     <tr>
                         <td>
                             <h3 className="cat-label">Lg Straight</h3>
@@ -128,6 +140,7 @@ function Categories(props) {
                         <td>Score Here</td>
                         <td>Score Here</td>
                     </tr>
+                {/* 5 of a kind row */}
                     <tr>
                         <td>
                             <h3 className="cat-label">*5 of a kind*</h3> 
@@ -138,6 +151,7 @@ function Categories(props) {
                         <td>Score Here</td>
                         <td>Score Here</td>
                     </tr>
+                {/* Chance row */}
                     <tr>
                         <td>
                             <h3 className="cat-label">Chance</h3>
@@ -147,6 +161,7 @@ function Categories(props) {
                         <td>Score Here</td>
                         <td>Score Here</td>
                     </tr>
+                {/* Total Score row */}
                     <tr>
                         <td>
                             <h3 className="cat-label">Total Score</h3>
