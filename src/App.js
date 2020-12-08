b80438a973691074f001637cfca31c3fd57cd48e
Aaron Garner <aaron.garner.1982@gmail.com>
2020-12-07 16:34:15 -0800

    Updated notes in app.js for full house function.

--- src/App.js
+++ src/App.js
@@ -100,9 +100,9 @@ rollCount = (event) => {
   }
 };
 
-// Function to count frequency of each number in dice array.
-//(need to make a frequency counter to get thr frequencies of each possible die value to use in finding full house, 3 of a kind, 4 of a kind, and 5 of a kind)
-dieFrequency = () => {
+// Function to calculate score for a full house.
+// ** (need to make a frequency counter to get thr frequencies of each possible die value to use in finding full house, 3 of a kind, 4 of a kind, and 5 of a kind)
+calcFullHouse = () => {
   
 };
 
